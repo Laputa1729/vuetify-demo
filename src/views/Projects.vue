8fccb375eaa2c3e02d6583f3194eafe60b729edb
Laputa <Laputa1729@163.com>
2022-11-01 23:05:44 +0800

    Expansion Panels.

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,7 +4,22 @@
 
         <!-- fluid属性：铺满 -->
         <v-container class="my-5">
-            content
+            <v-expansion-panels accordion>
+                <v-expansion-panel
+                    v-for="(project, index) in myProjects"
+                    :key="index"
+                >
+                    <v-expansion-panel-header>{{ project.title }}</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <v-card flat>
+                            <v-card-text class="pa-0">
+                                <div class="font-weight-bold">due by {{ project.due }}</div>
+                                <div>{{ project.content }}</div>
+                            </v-card-text>
+                        </v-card>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
         </v-container>
     </div>
 </template>
@@ -12,6 +27,24 @@
 <script>
 export default {
     name: 'Projects',
+    data() {
+        return {
+            projects: [
+                { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+                { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+                { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+                { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+                { title: 'Design a new website ...again', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur eius facilis ipsa laboriosam minima modi molestias, natus nisi nostrum quae quam quo ratione, sapiente similique temporibus totam ut voluptatum.' },
+            ]
+        }
+    },
+    computed: {
+        myProjects() {
+            return this.projects.filter(item => {
+                return item.person === 'The Net Ninja';
+            });
+        }
+    }
 }
 </script>
 
