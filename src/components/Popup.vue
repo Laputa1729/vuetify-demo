e6eff059b5fdaa64ac9b53e4c456fd02d91e40be
Laputa <Laputa1729@163.com>
2022-11-02 22:14:56 +0800

    v-form.

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -14,7 +14,22 @@
             <v-card-title>
                 <h2>Add a New Project</h2>
             </v-card-title>
-            <v-card-text>123</v-card-text>
+            <v-card-text>
+                <v-form class="px-3">
+                    <v-text-field
+                        prepend-icon="mdi-folder"
+                        label="Title"
+                        v-model="title"
+                    ></v-text-field>
+                    <v-textarea
+                        prepend-icon="mdi-pencil"
+                        label="Information"
+                        v-model="content"
+                    ></v-textarea>
+
+                    <v-btn text class="success mt-3" @click="submit">Add project</v-btn>
+                </v-form>
+            </v-card-text>
         </v-card>
     </v-dialog>
 </template>
@@ -22,6 +37,17 @@
 <script>
 export default {
     name: 'Popup',
+    data() {
+        return {
+            title: '',
+            content: ''
+        }
+    },
+    methods: {
+        submit() {
+            console.log(this.title, this.content)
+        }
+    }
 }
 </script>
 
